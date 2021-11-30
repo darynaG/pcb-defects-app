409865afa4a87ceea67dbfe79f6f13818163c93d
darynaG <dasha.gacosh@gmail.com>
2021-11-30 23:14:47 +0200

    Update utility.js

--- app/scripts/utility.js
+++ app/scripts/utility.js
@@ -29,7 +29,7 @@ function stop_load_display() {
 }
 
 az.read_local_file({
-    "file_path" : "../../test_temps/test_temps.json",
+    "file_path" : "pcb-defects-app/test_temps/test_temps.json",
     "done" : function(data) {
         az.hold_value.test_temps = Object.keys(data)
     }
